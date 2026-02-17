c23c0a69071c2b2634728fca56b19fc52b3acf55
RyzAdi <adityakumar03332@gmail.com>
2026-02-17 18:49:05 +0530

    adding physics and collisions

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,10 +2,10 @@ extends CharacterBody2D
 
 
 const SPEED = 150.0
-const JUMP_VELOCITY = -200.0
+const JUMP_VELOCITY = -350.0
 
 
-func move(a) -> void:
+func move(a):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * a
@@ -13,7 +13,7 @@ func move(a) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		print("wassup bro!!!")
+		
 
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
