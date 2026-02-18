0808894979b9db0b8af3710bee04c39d9123d4a8
RyzAdi <adityakumar03332@gmail.com>
2026-02-19 01:49:03 +0530

    adding host and join button

--- scenes/world.gd
+++ scenes/world.gd
@@ -1,4 +1,18 @@
 extends Node2D
 
 
-var peep = ENetMultiplayerPeer.new()
+var peer = ENetMultiplayerPeer.new()
+var port = 9992
+
+func _on_host_pressed() -> void:
+	peer.create_server(port)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(peer_added)
+	
+	
+func _on_join_pressed() -> void:
+	peer.create_client('localhost', port)
+	multiplayer.multiplayer_peer = peer
+	
+func peer_added(pid) -> void:
+	print('player ' + str(pid) + ' has joined!.')
