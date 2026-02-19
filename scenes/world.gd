c71ddaa1881d0863aa3b255582c41774d04f733c
RyzAdi <adityakumar03332@gmail.com>
2026-02-19 14:08:13 +0530

    adding the functionality

--- scenes/world.gd
+++ scenes/world.gd
@@ -4,15 +4,19 @@ extends Node2D
 var peer = ENetMultiplayerPeer.new()
 var port = 9992
 
+@onready var canvas1: CanvasLayer = $CanvasLayer
+
 func _on_host_pressed() -> void:
 	peer.create_server(port)
 	multiplayer.multiplayer_peer = peer
 	multiplayer.peer_connected.connect(peer_added)
+	canvas1.hide()
 	
 	
 func _on_join_pressed() -> void:
 	peer.create_client('localhost', port)
 	multiplayer.multiplayer_peer = peer
+	canvas1.hide()
 	
 func peer_added(pid) -> void:
 	print('player ' + str(pid) + ' has joined!.')
