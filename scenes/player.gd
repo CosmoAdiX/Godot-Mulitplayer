9aa08667651563d799613decb74ddf9f6e67e2cf
RyzAdi <adityakumar03332@gmail.com>
2026-02-21 18:25:05 +0530

    adding flip

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,9 +1,13 @@
 class_name Player
 extends CharacterBody2D
 
+@onready var m_player: CharacterBody2D = $player
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -350.0
+
+
+var flipy: bool = true
 
 func _enter_tree() -> void:
 	set_multiplayer_authority(int(str(name)))
@@ -17,8 +21,12 @@ func move(a):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
-
+		
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	if Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right"):
+		scale.x = -1
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
